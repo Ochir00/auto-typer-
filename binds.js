4f6ca9f337eeda00b452bcd4b6aced4ec18157f4
Ochir00 <mongolbucker@gmail.com>
2025-08-03 15:38:46 +0800

    text full typer

---
+++ binds.js
@@ -0,0 +1,38 @@
+module.exports = {
+  myFunc: () => {
+    var keyNames = {
+      "+": "add",
+      "-": "subtract",
+      " ": "space",
+      "&": "shift-ampersand",
+      "*": "shift-asterisk",
+      "@": "shift-at",
+      "`": "shift-back_quote space",
+      "~": "@514 space",
+      "^": "shift-circumflex space",
+      "\\": "back_slash",
+      "/": "slash",
+      "{": "shift-braceleft",
+      "}": "shift-braceright",
+      "[": "open_bracket",
+      "]": "close_bracket",
+      ":": "shift-colon",
+      ";": "semicolon",
+      ",": "comma",
+      $: "shift-dollar",
+      "€": "alt_gr-euro_sign",
+      "=": "equals",
+      "!": "shift-exclamation_mark",
+      "(": "shift-left_parenthesis",
+      ")": "shift-right_parenthesis",
+      "#": "shift-number_sign",
+      ".": "period",
+      "'": "quote",
+      '"': "shift-quotedbl",
+      _: "shift-underscore",
+      "|": "shift-@92",
+      "?": "shift-@47",
+    };
+    return keyNames;
+  },
+};
--- index.js
+++ index.js
@@ -1,23 +1,34 @@
 const ks = require("node-key-sender");
-
+const binds = require("./binds.js");
+const keyNames = binds.myFunc();
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-const text = "wh";
+const text = "auto typer 1.0v";
+const max = 100;
 const run = async () => {
   for (let i = 0; i < text.length; i++) {
-    await sleep(100);
-    ks.sendKey(text[i]);
+    if (text[i] == " ") {
+      await sleep(100);
+      ks.sendKey("space");
+    } else if(text[i] in keyNames){
+      await sleep(100);
+      ks.sendKey(keyNames[text[i]]);
+    } else {
+      await sleep(100);
+      ks.sendKey(text[i]);
+    }
   }
   await sleep(100);
   ks.sendKey("enter");
 };
+
 const start = async () => {
-    await sleep(3000);
-    while (true){
-        const randInt = Math.floor(Math.random() * 2000) + 1;
-        run()
-        await sleep(15000 + randInt);
-    }
-}
-start()
+  await sleep(3000);
+  for (let i = 1; i <= max; i++) {
+    const randInt = Math.floor(Math.random() * 4000) + 1;
+    run();
+    await sleep(15000 + randInt);
+  }
+};
+start();
