ba1d81321e7dbbe2b50a59c599ef76b6c4fd66a8
Ochir00 <mongolbucker@gmail.com>
2025-08-12 17:28:43 +0800

    helo auto typer good timer update

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const keyNames = binds.myFunc();
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-const text = "auto typer 1.0v";
-const max = 100;
+const text = "wh";
+const max = 1;
 const run = async () => {
   for (let i = 0; i < text.length; i++) {
     if (text[i] == " ") {
       await sleep(100);
       ks.sendKey("space");
-    } else if(text[i] in keyNames){
+    } else if (text[i] in keyNames) {
       await sleep(100);
       ks.sendKey(keyNames[text[i]]);
     } else {
@@ -27,8 +27,13 @@ const start = async () => {
   await sleep(3000);
   for (let i = 1; i <= max; i++) {
     const randInt = Math.floor(Math.random() * 4000) + 1;
+    console.log("while:::", i);
     run();
-    await sleep(15000 + randInt);
+    if (i == max) {
+      break;
+    } else {
+      await sleep(15000 + randInt);
+    }
   }
 };
 start();
